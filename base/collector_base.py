5f0f39427205c5f16fc77f65241cd66c340789d3
dvarfe <mgoverdon@gmail.com>
2025-11-10 20:29:58 +0300

    Update base collector

--- base/collector_base.py
+++ base/collector_base.py
@@ -1,11 +1,20 @@
 from abc import ABC, abstractmethod
+import os
+
 import pandas as pd
+
 
 class AbstractDataCollector(ABC):
     """Базовый класс для всех сборщиков данных"""
-    @abstractmethod
+
+    def __init__(self, config=None):
+        self.update_config(config or {})
+        # Имя файла для сохранения данных — по имени класса
+        self._csv_path = f"storage/data/{self.__class__.__name__}.csv"
+        os.makedirs(os.path.dirname(self._csv_path), exist_ok=True)
+
     def update_config(self, config):
-        pass
+        self.interval = config.get("interval", 1)  # сек между замерами
 
     @abstractmethod
     def find_objects(self):
