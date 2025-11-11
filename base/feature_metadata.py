9fe0b792a77ef7f76892e3571af8329614b94d7c
dvarfe <mgoverdon@gmail.com>
2025-11-11 22:11:13 +0300

    Add Feature Metadata type

---
+++ base/feature_metadata.py
@@ -0,0 +1,33 @@
+"""
+Общие типы и метаданные для всех коллекторов
+"""
+
+from enum import Enum
+from typing import Dict
+
+
+class FeatureType(Enum):
+    """Тип признака для определения способа отображения"""
+    NUMERICAL = "numerical"     # Числовой
+    CATEGORICAL = "categorical"  # Категориальный
+    TIMESTAMP = "timestamp"     # Временная метка
+    IDENTIFIER = "identifier"   # Идентификатор
+
+
+class FeatureMetadata:
+    """Метаданные признака"""
+
+    def __init__(self, name: str, feature_type: FeatureType, unit: str = "",
+                 description: str = ""):
+        self.name = name
+        self.type = feature_type
+        self.unit = unit
+        self.description = description
+
+    def to_dict(self) -> Dict[str, str]:
+        return {
+            "name": self.name,
+            "type": self.type.value,
+            "unit": self.unit,
+            "description": self.description,
+        }
